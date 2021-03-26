f2833e342ad6dbdfe00a44112219bf2b27d66b80
mizukitt1 <mizuki.tsu15@gmail.com>
2021-03-26 11:28:20 +0000

    [exercise]3-8

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -12,7 +12,7 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     if @book.save
-    redirect_to book_path(@book.id), notice: 'Book was successfully created.'
+      redirect_to book_path(@book.id), notice: 'Book was successfully created.'
     else 
       @books = Book.all
       render 'index'
@@ -26,8 +26,11 @@ class BooksController < ApplicationController
   
   def update
     @book = Book.find(params[:id])
-    @book.update(book_params)
-    redirect_to book_path(book)
+    if @book.update(book_params)
+      redirect_to book_path(@book.id), notice: 'Book was successfully update.'
+    else
+      render 'edit'
+    end
   end
 
   def destroy
